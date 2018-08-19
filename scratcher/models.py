3f3ae04d9519c4676b866efaf1bc954f293da832
Tommy <tommy@Tommys-MacBook-Pro.local>
2018-08-18 21:24:47 -0700

    add initial project

---
+++ scratcher/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    current_balance = models.FloatField()
+    saving_category = 
